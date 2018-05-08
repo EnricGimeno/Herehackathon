52393f0b34010df7c2b5faca12c519c1dcd8a241
engi <enric.gimeno.vilanova@gmail.com>
2018-05-08 15:14:52 +0200

    Template for Here Hackathon

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php require_once( "SNIPPETS/HEADER.php" ); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-4 ">
+            <h3>SlideBar</h3>
+        </div>
+        <div class="col-lg-8 col-md-8 ">
+            <h3>Map content</h3>
+        </div>
+    </div>
+</div>
+
+<?php require_once( "SNIPPETS/FOOTER.php" ); ?>
